bfd12ad97f5f028f7fb343dfe4e16ee5b7eee1a8
neomi buzaglo yoresh <neomibuz@gmail.com>
2022-03-23 12:35:06 +0200

    filter try

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,31 +1,6 @@
 <template>
   <header class="main-header">
     <h1 class="logo">Airyny</h1>
-
-    <div class="search-stay-place">
-      <input type="text" placeholder="Location" />|
-      <span class="demonstration">stay time</span>
-      <el-date-picker
-        v-model="value1"
-        type="daterange"
-        range-separator="To"
-        start-placeholder="Check in"
-        end-placeholder="Check out"
-      >
-      </el-date-picker
-      >|
-      <div
-        role="button"
-        tabindex="0"
-        aria-expanded="false"
-        data-testid="structured-search-input-field-guests-button"
-      >
-        <div class="_1qqq7vb">
-          <div class="_snpd7k">Guests</div>
-          <div class="_suql0c">Add guests</div>
-        </div>
-      </div>
-    </div>
   </header>
   <hr />
 </template>
@@ -35,26 +10,6 @@ import { ref } from "vue";
 
 export default {
   name: "stay-header",
-  data() {
-    return {
-      pickerOptions: {
-        shortcuts: [
-          {
-            text: "Last week",
-            onClick(picker) {
-              const end = new Date();
-              const start = new Date();
-              start.setTime(
-                start.getTime() - 3600 * 1000 * 24 * 7
-              );
-              picker.$emit("pick", [start, end]);
-            },
-          },
-        ],
-      },
-      value1: "",
-    };
-  },
 };
 </script>
 
--- src/components/stay-filter.vue
+++ src/components/stay-filter.vue
@@ -1,7 +1,53 @@
-<template></template>
+<template>
+  <div class="search-stay-place">
+    <input type="text" placeholder="Location" />|
+    <span class="demonstration">stay time</span>
+    <el-date-picker
+      v-model="value1"
+      type="daterange"
+      range-separator="To"
+      start-placeholder="Check in"
+      end-placeholder="Check out"
+    >
+    </el-date-picker
+    >|
+    <div
+      role="button"
+      tabindex="0"
+      aria-expanded="false"
+      data-testid="structured-search-input-field-guests-button"
+    >
+      <div>
+        <div class="_snpd7k">Guests</div>
+        <div class="_suql0c">Add guests</div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      pickerOptions: {
+        shortcuts: [
+          {
+            text: "Last week",
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(
+                start.getTime() - 3600 * 1000 * 24 * 7
+              );
+              picker.$emit("pick", [start, end]);
+            },
+          },
+        ],
+      },
+      value1: "",
+    };
+  },
+};
 </script>
 
 <style></style>
