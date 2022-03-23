dc270c79ffde35582397a4667c046a89e14e65cc
neomi buzaglo yoresh <neomibuz@gmail.com>
2022-03-23 10:23:54 +0200

    header dates

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,14 +1,50 @@
 <template>
   <header class="main-header">
-    <h1>Airyny</h1>
+    <h1 class="logo">Airyny</h1>
 
-    <div aria-labeledby=""></div>
+    <div class="search-stay-place">
+      <input type="text" placeholder="Location" />|
+      <span class="demonstration">stay time</span>
+      <el-date-picker
+        v-model="value1"
+        type="daterange"
+        range-separator="To"
+        start-placeholder="Check in"
+        end-placeholder="Check out"
+      >
+      </el-date-picker
+      >|
+    </div>
   </header>
   <hr />
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+
+export default {
+  name: "stay-header",
+  data() {
+    return {
+      pickerOptions: {
+        shortcuts: [
+          {
+            text: "Last week",
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(
+                start.getTime() - 3600 * 1000 * 24 * 7
+              );
+              picker.$emit("pick", [start, end]);
+            },
+          },
+        ],
+      },
+      value1: "",
+    };
+  },
+};
 </script>
 
 <style></style>
