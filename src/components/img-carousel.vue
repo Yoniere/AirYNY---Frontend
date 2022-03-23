0b04795766223106b5a518d61890a98af80f7d4f
Yonatan Arm <yoniarm123@gmail.com>
2022-03-23 21:30:52 +0200

    nice

--- src/components/img-carousel.vue
+++ src/components/img-carousel.vue
@@ -1,26 +1,28 @@
 <template>
-  <el-carousel :trigger="click" indicator-position="outside" height="200px" :autoplay="false"  >
-    <el-carousel-item v-for="img in imgs" :key="img" >
- <img :src ="`https://res.cloudinary.com/yonatan-cajan22/image/upload/v1648031221/airyny/${img}`" alt="imgStay" /> 
+  <el-carousel
+    :trigger="click"
+    indicator-position="inside"
+    height=100%
+    :autoplay="false"
+  >
+    <el-carousel-item v-for="img in imgs" :key="img">
+      <img
+        :src="`https://res.cloudinary.com/yonatan-cajan22/image/upload/v1648031221/airyny/${img}`"
+        alt="imgStay"
+      />
     </el-carousel-item>
   </el-carousel>
 </template>
 
-
-
 <script>
-
-
 export default {
- props:{
-    imgs:{
-        type:Array,
-        
-    }
-},
-}
+  props: {
+    imgs: {
+      type: Array,
+    },
+  },
+};
 </script>
-
 
 <style>
 .el-carousel {
@@ -28,6 +30,10 @@ export default {
   margin: 0;
   text-align: center;
 }
+.el-carousel__button{
+  z-index: 10;
+   border-radius: 30%;
+    width: 0.7em  !important;
 
-
+}
 </style>
--- src/components/stay-preview.vue
+++ src/components/stay-preview.vue
@@ -1,8 +1,33 @@
 <template>
-     <li class="stay-card" @click="goToDeatails">
-        <section class="flex-col"> 
-       <img-carousel :imgs="stay.imgUrls"/>
-        <div class="flex-row">
+     <li class="stay-card flex-col" @click="goToDeatails">
+              <span class="icon-heart"> 
+                 <svg
+        viewBox="0 0 32 32"
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+        role="presentation"
+        focusable="false"
+        style="
+          display: block;
+          fill: transparent;
+          height: 24px;
+          width: 24px;
+          stroke-width: 2;
+          overflow: visible;
+          stroke: black
+        "
+      >
+        <path
+          d="m16 28c7-4.733 14-10 14-17 0-1.792-.683-3.583-2.05-4.95-1.367-1.366-3.158-2.05-4.95-2.05-1.791
+           0-3.583.684-4.949 2.05l-2.051 2.051-2.05-2.051c-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949
+            2.05-1.367 1.367-2.051 3.158-2.051 4.95 0 7 7 12.267 14 17z"
+        ></path>
+
+      </svg>
+      </span>
+       <img-carousel :imgs="stay.imgUrls" class="card-img"/>
+    <article class="flex-col">
+      <div class="flex-row card-rate">
          <svg
         viewBox="0 0 32 32"
         xmlns="http://www.w3.org/2000/svg"
@@ -20,17 +45,15 @@
             7.293-6.565a1 1 0 0 0-.541-1.735l-9.86-1.271-4.127-8.885a1 1 0 0 0-1.814 0z"
           fill-rule="evenodd"
         ></path>
-      </svg>{{getRating}}
-            <span>  ({{stay.numOfReviews}}) </span>
-       </div>
-       <div>
-             <span>{{ stay.name }}  </span>
-       </div>
-       <div>
-             <span>{{stay.price }}$ / night </span>
-       </div>
-     </section>
-  </li>
+      </svg>{{getRating}} ({{stay.numOfReviews}})
+      </div>
+      <p class="card-detail flex-col">
+               <span>{{ stay.propertyType}} <span> &#183;</span> {{stay.address.city}}  </span>
+               <span>{{stay.name }} </span>
+              <span class="bold">${{stay.price }}/night </span>
+      </p>
+    </article>
+</li>
   
 </template>
 
