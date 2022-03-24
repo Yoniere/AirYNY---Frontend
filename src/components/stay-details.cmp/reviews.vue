03f1a10187c3dd5ef85f7c3e5ac759723d9b01d4
Yonatan Erez <jontaner12@gmail.com>
2022-03-24 22:09:47 +0200

    more details page

---
+++ src/components/stay-details.cmp/reviews.vue
@@ -0,0 +1,53 @@
+<template>
+
+  <section class="stay-details-reviews main-layout">
+      <div class="flex">
+            <svg class="star"
+            viewBox="0 0 32 32"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            role="presentation"
+            focusable="false"
+            style="display: block; height: 14px; width: 14px; fill: #ff385c"
+          >
+            <path
+              d="M15.094 1.579l-4.124 8.885-9.86 1.27a1 1 0 0 0-.542 1.736l7.293 6.565-1.965
+           9.852a1 1 0 0 0 1.483 1.061L16 25.951l8.625 4.997a1 1 0 0 0 1.482-1.06l-1.965-9.853
+            7.293-6.565a1 1 0 0 0-.541-1.735l-9.86-1.271-4.127-8.885a1 1 0 0 0-1.814 0z"
+              fill-rule="evenodd"
+            ></path>
+          </svg>
+        <span class="rating">{{ getRating }}</span> ·
+        <button class="review-btn">
+          <span> {{ stay.numOfReviews }}</span>
+          reviews
+        </button>
+        </div>
+        <div v-for="(value,key) in stay.reviewScores" :key="value">
+            <div>{{key}} {{value/2}}</div>
+        </div>
+        </section>
+</template>
+
+<script>
+export default {
+  name: "reviews",
+  props: {
+    stay: {
+      type: Object,
+    },
+  },
+  createad() {
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    getRating() {
+      return this.stay.reviewScores.rating / 10;
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/stay-details.vue
+++ src/views/stay-details.vue
@@ -4,8 +4,8 @@
     <main-amenities :stay="stay"></main-amenities>
     <stay-description :stay="stay"></stay-description>
     <amenities :stay="stay"></amenities>
-
-
+    <reviews :stay="stay"></reviews>
+    
   </section>
 </template>
 
@@ -15,6 +15,7 @@ import ImgsComp from "../components/stay-details.cmp/imgs-comp.vue";
 import mainAmenities from "../components/stay-details.cmp/main-amenities.vue"
 import stayDescription from "../components/stay-details.cmp/stay-description.vue";
 import amenities from "../components/stay-details.cmp/amenities.vue";
+import reviews from "../components/stay-details.cmp/reviews.vue";
 
 export default {
   data() {
@@ -32,7 +33,9 @@ export default {
     ImgsComp,
     stayDescription,
     mainAmenities,
-    amenities
+    amenities,
+    reviews
+
 },
   computed: {},
 };
