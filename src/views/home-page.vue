dea4c282bfb38d8316a593163d52b9f5c84dc356
neomi buzaglo yoresh <neomibuz@gmail.com>
2022-03-25 15:26:49 +0300

    try fonts

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -6,7 +6,21 @@
         <h3>I'm flexible</h3>
       </button>
     </section>
-
+    <div>
+      <p class="ac">roboto-black</p>
+      <p class="b">roboto-BlackItalic</p>
+      <p class="c">roboto-bold</p>
+      <p class="d">roboto-BoldItalic</p>
+      <p class="e">roboto-italic</p>
+      <p class="f">roboto-light</p>
+      <p class="g">roboto-light-italic</p>
+      <p class="h">roboto-Medium</p>
+      <p class="i">roboto-Medium-italic</p>
+      <p class="j">roboto-regular</p>
+      <p class="q">roboto-thin</p>
+      <p class="l">roboto-thin-italic</p>
+      <p class="m">brown-regular</p>
+    </div>
     <home-list :stay="ratedStays" />
     <section class="host-container flex main-layout">
       <h1>Become a host</h1>
