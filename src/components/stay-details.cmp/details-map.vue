9dc874ae098a9769a89b8d731183f78214211969
neomi buzaglo yoresh <neomibuz@gmail.com>
2022-03-26 11:29:03 +0300

    first map

--- src/components/stay-details.cmp/details-map.vue
+++ src/components/stay-details.cmp/details-map.vue
@@ -1,18 +1,43 @@
 <template>
-
+  <hr />
+  <!-- <main class="map-details"> -->
+  <GMapMap :center="center" :zoom="7" map-type-id="terrain">
+    <GMapCluster>
+      <GMapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+        @click="center = m.position"
+      />
+    </GMapCluster>
+  </GMapMap>
+  <!-- </main> -->
 </template>
 
 <script>
 export default {
-      name: "mapDetails",
+  name: "mapDetails",
   props: {
     stay: {
       type: Object,
     },
   },
+  data() {
+    return {
+      center: { lat: 51.093048, lng: 6.84212 },
+      markers: [
+        {
+          position: {
+            lat: 51.093048,
+            lng: 6.84212,
+          },
+        }, // Along list of clusters
+      ],
+    };
+  },
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
