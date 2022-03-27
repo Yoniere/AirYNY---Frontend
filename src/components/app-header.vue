929c01f38c9109327c7b79c99b0fa51ffd2f1d9a
neomi buzaglo yoresh <neomibuz@gmail.com>
2022-03-27 10:22:05 +0300

    new pic

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,31 +1,62 @@
 <template>
-  <section class="main-header" :class="this.stickyNav ? 'fix' : 'fullHeader' ">
-      <div class="flex space-between header-features main-layout">
+  <section
+    class="main-header"
+    :class="this.stickyNav ? 'fix' : 'fullHeader'"
+  >
+    <div
+      class="flex space-between header-features main-layout"
+    >
       <div class="logo-container">
-        <router-link to="/" ><img class="logo flex align-center" :src="getLogo"></router-link>
+        <router-link to="/"
+          ><img
+            class="logo flex align-center"
+            :src="getLogo"
+        /></router-link>
       </div>
-        <button class="mini-filter flex space-between align-center clickable " :class="(this.openfilter) ? 'hide' : 'fixed'"
-        @click="toggle">
-         <span> {{getFilter}}</span> 
+      <button
+        class="mini-filter flex space-between align-center clickable"
+        :class="this.openfilter ? 'hide' : 'fixed'"
+        @click="toggle"
+      >
+        <span> {{ getFilter }}</span>
 
-<div class="search-btn">
-      <button @click="setFilter">
-        <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" 
-        style="display:block;fill:none;height:12px;width:12px;stroke:white;stroke-width:5;overflow:visible" 
-        aria-hidden="true" role="presentation" focusable="false"><g fill="none">
-          <path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 
-          0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path></g></svg>
+        <div class="search-btn">
+          <button @click="setFilter">
+            <svg
+              viewBox="0 0 32 32"
+              xmlns="http://www.w3.org/2000/svg"
+              style="
+                display: block;
+                fill: none;
+                height: 12px;
+                width: 12px;
+                stroke: white;
+                stroke-width: 5;
+                overflow: visible;
+              "
+              aria-hidden="true"
+              role="presentation"
+              focusable="false"
+            >
+              <g fill="none">
+                <path
+                  d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 
+          0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"
+                ></path>
+              </g>
+            </svg>
+          </button>
+        </div>
       </button>
-      </div>
-           </button>
 
-
-
-      <div class="flex align-center menu-btns" :class="this.stickyNav ? 'header-fix' : '' ">
+      <div
+        class="flex align-center menu-btns"
+        :class="this.stickyNav ? 'header-fix' : ''"
+      >
         <router-link to="/stay">Explore </router-link>
         <router-link to="/">Become a host </router-link>
         <button class="world-btn">
-           <img
+          <img
             class="world-img"
             src="https://res.cloudinary.com/yonatan-cajan22/image/upload/v1648059841/airyny/world-icon.svg"
           />
@@ -41,16 +72,17 @@
           />
         </button>
       </div>
-      </div>
-  
-    <stay-filter :class="(openfilter) ? '' : 'hide'" @filterd="setMiniFilter"/>
+    </div>
 
-  
+    <stay-filter
+      :class="openfilter ? '' : 'hide'"
+      @filterd="setMiniFilter"
+    />
   </section>
 </template>
 
 <script>
-import stayFilter from './stay-filter.vue';
+import stayFilter from "./stay-filter.vue";
 
 export default {
   name: "stay-header",
@@ -58,49 +90,48 @@ export default {
   components: {
     stayFilter,
   },
-  created () {
-    window.addEventListener('scroll', this.handleScroll);
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
   },
-  data(){
-    return{
-   stickyNav: false,
-   openfilter: true,
-   filter:null,
-    }
+  data() {
+    return {
+      stickyNav: false,
+      openfilter: true,
+      filter: null,
+    };
   },
 
- 
-  methods:{
- handleScroll (event) {   
-  if (window.top.scrollY > 20 ) {
-    this.stickyNav = true
-    this.openfilter = false
-    
-  } else {
-    this.stickyNav = false
-     this.openfilter = true
-  }
-},
-toggle(){
-    this.openfilter = !this.openfilter
-},
-setMiniFilter(filterBy){
-  this.filter = filterBy
-
-}
-
-  },
-  computed:{
-    getLogo(){
-      return this.stickyNav ? 'https://res.cloudinary.com/yonatan-cajan22/image/upload/v1648055648/airyny/logo2.png' : 'https://res.cloudinary.com/yonatan-cajan22/image/upload/v1648055648/airyny/logo1.png'
+  methods: {
+    handleScroll(event) {
+      if (window.top.scrollY > 20) {
+        this.stickyNav = true;
+        this.openfilter = false;
+      } else {
+        this.stickyNav = false;
+        this.openfilter = true;
+      }
     },
-    getFilter(){
-     return  this.filter ? `${this.filter}` :  'Start to search'
-    }
-
+    toggle() {
+      this.openfilter = !this.openfilter;
+    },
+    setMiniFilter(filterBy) {
+      this.filter = filterBy;
+    },
   },
-   unmounted () {
-    window.removeEventListener('scroll', this.handleScroll);
+  computed: {
+    getLogo() {
+      return this.stickyNav
+        ? "https://res.cloudinary.com/yonatan-cajan22/image/upload/v1648055648/airyny/logo2.png"
+        : "https://res.cloudinary.com/yonatan-cajan22/image/upload/v1648055648/airyny/logo1.png";
+    },
+    getFilter() {
+      return this.filter
+        ? `${this.filter}`
+        : "Start to search";
+    },
+  },
+  unmounted() {
+    window.removeEventListener("scroll", this.handleScroll);
   },
 };
 </script>
