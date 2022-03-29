28f7fc7a54fa06d6d5b7a8e69c3aa9eb9338501a
Yonatan Erez <jontaner12@gmail.com>
2022-03-29 18:06:33 +0300

    ff

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,6 @@ const router = createRouter({
     history: createWebHashHistory(),
     linkActiveClass: 'active',
 
-<<<<<<< HEAD
     routes: [{
             path: '/',
             name: 'home',
@@ -36,39 +35,11 @@ const router = createRouter({
             name: 'user-dashboard',
             component: userDashboard,
         },
-=======
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: homePage
-    },
-    {
-      path: '/stay',
-      name: 'explore-page',
-      component: explorePage
-    },
-    {
-      path: '/stay/:id',
-      name: 'stay-details',
-      component: stayDetails
-    },
-    {
-      path: '/user/:id',
-      name: 'user-details',
-      component: userDetails,
-    },
-    {
-      path: '/dashboard',
-      name: 'user-dashboard',
-      component: userDashboard,
-    },
-    {
-      path: '/wishList',
-      name: 'user-wish-list',
-      component: userWishList,
-    },
->>>>>>> 1652eb066b1a5ef5979cc6105e74246935b403cc
+        {
+            path: '/wishList',
+            name: 'user-wish-list',
+            component: userWishList,
+        },
 
 
 
