ccac8a22244b6465c84bbfddc54073d9572bd892
neomi buzaglo yoresh <neomibuz@gmail.com>
2022-03-30 23:32:38 +0300

    fix host pix

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,30 +1,30 @@
-export const focusDirective = {
-  mounted: (el) => {
-    el.focus()
-  },
-}
+// export const focusDirective = {
+//   mounted: (el) => {
+//     el.focus()
+//   },
+// }
 
-export const closeDirevtive = {
-  mounted: function (el, expression, context) {
-    console.log({ expression }, { context });
-    el.ClickOutSide = ({ clientX, clientY }) => {
-      var { left, top, width, height } = el.getBoundingClientRect()
-      if (!(clientX > left && clientX < left + width && clientY > top && clientY < top + height)) {
-        console.log('hi');
-        console.log(expression);
-        console.log(context);
-        context[expression]()
-      }
-    }
-    setTimeout(() => {
-      document.addEventListener('click', el.ClickOutSide)
-    }, 0)
-  },
+// export const closeDirevtive = {
+//   mounted: function (el, expression, context) {
+//     console.log({ expression }, { context });
+//     el.ClickOutSide = ({ clientX, clientY }) => {
+//       var { left, top, width, height } = el.getBoundingClientRect()
+//       if (!(clientX > left && clientX < left + width && clientY > top && clientY < top + height)) {
+//         console.log('hi');
+//         console.log(expression);
+//         console.log(context);
+//         context[expression]()
+//       }
+//     }
+//     setTimeout(() => {
+//       document.addEventListener('click', el.ClickOutSide)
+//     }, 0)
+//   },
 
-  unmounted: (el) => {
-    document.removeEventListener('click', el.ClickOutSide)
-  }
-}
+//   unmounted: (el) => {
+//     document.removeEventListener('click', el.ClickOutSide)
+//   }
+// }
 
 
 
