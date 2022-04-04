20e0c1b3bb97c9793e7afb30c198b612489f6cdc
neomi buzaglo yoresh <neomibuz@gmail.com>
2022-04-04 09:14:55 +0300

    try dot not

--- src/components/user-deatils-modal.vue
+++ src/components/user-deatils-modal.vue
@@ -2,8 +2,9 @@
   <section>
     <section v-if="userLogged" class="user-modal flex-col">
       <router-link to="/"> Messages</router-link>
-     <router-link to="/"> Trips</router-link>
+      <router-link to="/"> Trips</router-link>
       <router-link to="/wishList"> Wish List</router-link>
+
       <router-link to="/dashboard"> Dashboard</router-link>
       <router-link to="/"> Account</router-link>
       <span @click="doLogout">Logout</span>
@@ -20,7 +21,7 @@
 
 <script>
 import { userService } from "../services/user-service.js";
-
+import { Share } from "@element-plus/icons-vue";
 export default {
   data() {
     return {
@@ -28,7 +29,8 @@ export default {
     };
   },
   async created() {
-    const loggedInUser = await userService.getLoggedinUser();
+    const loggedInUser =
+      await userService.getLoggedinUser();
     if (!loggedInUser) this.userLogged = false;
     else {
       this.userLogged = true;
@@ -42,7 +44,7 @@ export default {
       this.$emit("closeModalDetails");
       this.$store.dispatch({ type: "logout" });
       this.$router.push("/");
-      window.scrollTo( 0,  0);
+      window.scrollTo(0, 0);
     },
   },
   components: {},
