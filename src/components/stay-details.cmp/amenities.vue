92c2588a9fd3fc2bd65756a35e5ed92bef81c6b6
Yonatan Arm <yoniarm123@gmail.com>
2022-05-09 12:15:58 +0300

    clean-code

--- src/components/stay-details.cmp/amenities.vue
+++ src/components/stay-details.cmp/amenities.vue
@@ -3,10 +3,6 @@
     <h1 class="title">What this place offers</h1>
     <div class="amenities flex">
       <div class="amenity flex">
-        <!-- <img
-        class="amenity-img"
-        src="https://res.cloudinary.com/yonatan-cajan22/image/upload/v1648031221/airyny/tv24-icon.png"
-      /> -->
         <svg
           class="amenity-img"
           viewBox="0 0 32 32"
@@ -29,10 +25,6 @@
       </div>
 
       <div class="amenity flex">
-        <!-- <img
-          class="amenity-img"
-          src="https://res.cloudinary.com/yonatan-cajan22/image/upload/v1648031221/airyny/kitchen-icon.png"
-        /> -->
         <svg
           class="amenity-img"
           viewBox="0 0 32 32"
@@ -73,18 +65,10 @@
             d="M30 19v8h-2v-8zm-4 0v8H3a2 2 0 0 1-1.995-1.85L1 25v-4a2 2 0 0 1 1.85-1.995L3 19zm-2 2H3v4h21zM21 3a4 4 0 0 1 3.356 6.178l-.1.145.137.041a8.004 8.004 0 0 1 5.603 7.387L30 17h-2a6 6 0 0 0-5.775-5.996L22 11h-1V9a2 2 0 0 0 .15-3.995L21 5zm-4 0v2a4 4 0 0 0-.2 7.995L17 13h5a4 4 0 0 1 3.995 3.8L26 17h-2a2 2 0 0 0-1.85-1.995L22 15h-5a6 6 0 0 1-.225-11.996z"
           ></path>
         </svg>
-        <!-- <img
-          class="amenity-img"
-          src="https://res.cloudinary.com/yonatan-cajan22/image/upload/v1648031221/airyny/smoke-icon.png"
-        /> -->
         <p class="amenity-title">Smoking Allowed</p>
       </div>
 
       <div class="amenity flex">
-        <!-- <img
-          class="amenity-img"
-          src="https://res.cloudinary.com/yonatan-cajan22/image/upload/v1648031221/airyny/wifi32-icon.png"
-        /> -->
         <svg
           class="amenity-img"
           viewBox="0 0 32 32"
@@ -107,11 +91,6 @@
       </div>
 
       <div class="amenity flex">
-        <!-- <img
-          class="amenity-img"
-          src="https://res.cloudinary.com/yonatan-cajan22/image/upload/v1648031221/airyny/ac30-icon.png"
-        /> -->
-
         <svg
           class="amenity-img"
           viewBox="0 0 32 32"
@@ -134,10 +113,6 @@
       </div>
 
       <div class="amenity flex">
-        <!-- <img
-          class="amenity-img"
-          src="https://res.cloudinary.com/yonatan-cajan22/image/upload/v1648031221/airyny/tv24-icon.png"
-        /> -->
         <svg
           class="amenity-img"
           viewBox="0 0 32 32"
